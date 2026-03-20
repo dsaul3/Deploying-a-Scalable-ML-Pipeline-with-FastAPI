9af01bff8c79fab0360f30448b2ce015d1699fcd
dsaul3 <dsaul3@wgu.edu>
2026-03-20 12:47:07 -0700

    Unit test successful

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,38 @@
 import pytest
-# TODO: add necessary import
+# add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics
+from ml.data import apply_label
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+# Test 1: train_model returns RandomForestClassifier
+def test_train_model():
     """
-    # add description for the first test
+    # this test is to ensure the proper model is returned
     """
-    # Your code here
-    pass
+    X_train = np.array([[1,2], [3,4], [5,6], [7,8], [9,10], [11,12]])
+    y_train = np.array([0, 1, 0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
+# Test 2: compute_model_metrics returns expected values
+def test_compute_model_metrics():
+    """
+    # this test is to ensure that the predicition model
+    will return expected values for a perfect prediction
+    """
+    y_true = np.array([1, 1, 0, 0, 1])
+    y_pred = np.array([1, 1, 0, 0, 1])
+    precision_score, recall_score, fbeta_score = compute_model_metrics(y_true, y_pred)
+    assert precision_score == pytest.approx(1.0)
+    assert recall_score == pytest.approx(1.0)
+    assert fbeta_score == pytest.approx(1.0)
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+# Test 3: apply_label returns correct string
+def test_apply_label():
     """
-    # add description for the second test
+    # this test is to ensure the apply_label returns
+    the expected strings
     """
-    # Your code here
-    pass
-
-
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+    assert apply_label([0]) == "<=50K"
+    assert apply_label([1]) == ">50K"
